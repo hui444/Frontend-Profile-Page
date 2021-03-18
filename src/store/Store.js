6ca2868517d8975625738184f07993f83c774ed0
hui444 <ygreek@gmail.com>
2021-03-18 17:01:33 +0800

    Add local storage and persist redux state

--- src/store/Store.js
+++ src/store/Store.js
@@ -2,14 +2,26 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { profile } from "./profile/reducer";
-
-export const rootReducer = combineReducers({ profile });
+import { loadState, saveState } from "./localStorage";
+import throttle from "lodash.throttle";
 
 const middlewares = [thunk];
+
+const persistedState = loadState();
+
+export const rootReducer = combineReducers({ profile, persistedState });
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      todos: store.getState().todos,
+    });
+  }, 1000)
 );
 
 export default store;
