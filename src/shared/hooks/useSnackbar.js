a694ba5157be91b2d5e644edcf7fd5c175eb3ca2
hui444 <ygreek@gmail.com>
2021-03-20 15:50:24 +0800

    Add useSnackbar hook

---
+++ src/shared/hooks/useSnackbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { message } from "antd";
+
+import "antd/dist/antd.css";
+
+const useSnackbar = (type) => {
+  const success = (content) => {
+    message.success(content);
+  };
+
+  const warning = (content) => {
+    message.warning(content);
+  };
+
+  const error = (content) => {
+    message.error(content);
+  };
+
+  if (type === "success") {
+    return [success];
+  } else if (type === "warning") {
+    return [warning];
+  } else {
+    return [error];
+  }
+};
+
+export default useSnackbar;
