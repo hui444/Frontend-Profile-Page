98531a5804acac7d7f36bb86c5f08a5fc1e95230
hui444 <ygreek@gmail.com>
2021-03-20 15:40:51 +0800

    Update profile reducer

--- src/store/profile/reducer.js
+++ src/store/profile/reducer.js
@@ -1,11 +1,17 @@
 const initialState = {
-  isLoading: true,
+  isLoading: false,
   errorMessage: undefined,
+  profile: undefined,
+  workExperience: undefined,
+  allWorkExperiences: undefined,
 };
 
 export const ACTION = {
   SET_IS_LOADING: "SET_IS_LOADING",
   SET_ERROR_MESSAGE: "SET_ERROR_MESSAGE",
+  GET_PROFILE_BY_PID: "GET_PROFILE_BY_PID",
+  GET_WORK_EXPERIENCE_BY_WEID: "GET_WORK_EXPERIENCE_BY_WEID",
+  GET_ALL_WORK_EXPERIENCE_BY_PID: "GET_ALL_WORK_EXPERIENCE_BY_PID",
 };
 
 export const profile = (state = initialState, action) => {
@@ -20,6 +26,24 @@ export const profile = (state = initialState, action) => {
       return {
         ...state,
         errorMessage: action.errorMessage,
+      };
+    }
+    case ACTION.GET_PROFILE_BY_PID: {
+      return {
+        ...state,
+        profile: action.profile,
+      };
+    }
+    case ACTION.GET_WORK_EXPERIENCE_BY_WEID: {
+      return {
+        ...state,
+        workExperience: action.workExperience,
+      };
+    }
+    case ACTION.GET_ALL_WORK_EXPERIENCE_BY_PID: {
+      return {
+        ...state,
+        allWorkExperiences: action.allWorkExperiences,
       };
     }
     default:
